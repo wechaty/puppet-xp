301938c2d98d5c4657c6728b89df04fddcd47b62
Huan <zixia@zixia.net>
2021-07-23 02:48:43 +0800

    add ripe wechaty example

---
+++ examples/ripe-wechaty.ts
@@ -0,0 +1,60 @@
+/**
+ * Wechaty - Conversational RPA SDK for Chatbot Makers.
+ *  - https://github.com/wechaty/wechaty
+ */
+import {
+  Contact,
+  Message,
+  ScanStatus,
+  Wechaty,
+  log,
+}                  from 'wechaty'
+
+import { PuppetXp } from '../src/puppet-xp'
+
+function onScan (qrcode: string, status: ScanStatus) {
+  if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
+    const qrcodeImageUrl = [
+      'https://wechaty.js.org/qrcode/',
+      encodeURIComponent(qrcode),
+    ].join('')
+
+    log.info('StarterBot', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcodeImageUrl)
+  } else {
+    log.info('StarterBot', 'onScan: %s(%s)', ScanStatus[status], status)
+  }
+}
+
+function onLogin (user: Contact) {
+  log.info('StarterBot', '%s login', user)
+}
+
+function onLogout (user: Contact) {
+  log.info('StarterBot', '%s logout', user)
+}
+
+async function onMessage (msg: Message) {
+  log.info('StarterBot', msg.toString())
+
+  if (msg.text() === 'ding') {
+    await msg.say('dong')
+  }
+}
+
+const puppet = new PuppetXp()
+const bot = new Wechaty({
+  name: 'ding-dong-bot',
+  puppet,
+  // puppetOptions: {
+  //   token: 'xxx',
+  // }
+})
+
+bot.on('scan',    onScan)
+bot.on('login',   onLogin)
+bot.on('logout',  onLogout)
+bot.on('message', onMessage)
+
+bot.start()
+  .then(() => log.info('StarterBot', 'Starter Bot Started.'))
+  .catch(e => log.error('StarterBot', e))
